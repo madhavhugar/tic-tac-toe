3ce0123cbc9c9e7f4c123505e5676e5ceef0ebc0
Madhav Hugar <madhav.hugar@gmail.com>
2018-02-12 16:40:02 +0100

    added game winning checks

--- src/main/scala/BoardStandard.scala
+++ src/main/scala/BoardStandard.scala
@@ -1,5 +1,7 @@
 package tictactoe
 
+import com.sun.xml.internal.ws.dump.LoggingDumpTube.Position
+
 
 /**
   * BoardStandard represents the standard 3X3 TicTacToe board
@@ -8,6 +10,9 @@ private class BoardStandard() {
 
   /** A predefined standard board has 3X3 matrix */
   val board: Array[Array[Int]] = Array.ofDim[Int](3, 3)
+
+  /** Both the player moves */
+  var playersMovesSet: Set[(Int, Int)] = Set()
 
   /** Initializes all the matix elements to 999*/
   def initializeBoard(): Unit = {
@@ -18,6 +23,62 @@ private class BoardStandard() {
     }
   }
 
+  def positionTaken(position: (Int, Int)): Boolean = {
+    assert(position._1 < 3 && position._2 < 3)
+    if(playersMovesSet.contains(position)) {
+      println("Invalid or occupied position")
+      true
+    } else {
+      playersMovesSet = playersMovesSet + position
+      false
+    }
+  }
+
+  def checkWinner(player: Player): Boolean = {
+    val moves = player.getMovesList
+
+    // Checks for row entries
+      for (i <- 0 until 3) {
+        var count = 0
+        for (j <- 0 until 3) {
+          if (moves.contains(i, j))
+            count += 1
+        }
+        print(".")
+        if (count == 3) return true
+      }
+
+      // Checks for column entries
+      for (j <- 0 until 3) {
+        var count = 0
+        for (i <- 0 until 3) {
+          if (moves.contains(i, j))
+            count += 1
+        }
+        print(".")
+        if (count == 3) return true
+      }
+
+    // Checks for diagonal entries
+      var count = 0
+      for (i <- 0 until 3) {
+        if (moves.contains(i, i))
+          count += 1
+      }
+      print(".")
+      if (count == 3) return true
+      count = 0
+      for (i <- 0 until 3) {
+        for (j <- 2 to 0) {
+          if (moves.contains(i, j))
+            count += 1
+        }
+      }
+      print(".")
+      if (count == 3) return true
+
+    false
+  }
 
   /** Console prints the board */
   def printBoard(x: Player,o: Player): Unit = {
