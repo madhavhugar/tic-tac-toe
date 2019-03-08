c56f1c7646f9112ff0adc0ca8a010c07621909af
Madhav Hugar <madhav.hugar@gmail.com>
2019-03-08 04:34:31 +0100

    Check if previous position has been taken

--- README.md
+++ README.md
@@ -2,9 +2,4 @@
 
 The game of Tic-Tac-Toe in Scala
 
-## Development features
-
-* Console based input game
-* GUI based input game
-* Store player records in DB
-* Host Tic-Tac-Toe game server (ensuring the app is multi-threaded)
+![The game of Tic-Tac-Toe in Scala](./tic-tac-toe.png)
--- src/main/scala/BoardStandard.scala
+++ src/main/scala/BoardStandard.scala
@@ -8,20 +8,11 @@ import com.sun.xml.internal.ws.dump.LoggingDumpTube.Position
   */
 private class BoardStandard() {
 
-  /** A predefined standard board has 3X3 matrix */
-  val board: Array[Array[Int]] = Array.ofDim[Int](3, 3)
+
 
   /** Both the player moves */
   var playersMovesSet: Set[(Int, Int)] = Set()
 
-  /** Initializes all the matix elements to 999*/
-  def initializeBoard(): Unit = {
-    for (i <- 0 until 3) {
-      for (j <- 0 until 3) {
-        board(i)(j) = 999
-      }
-    }
-  }
 
   /** Checks if the entered position is invalid or has already been taken*/
   def positionTaken(position: (Int, Int)): Boolean = {
